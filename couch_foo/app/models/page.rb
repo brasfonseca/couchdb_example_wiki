660a8e87c3b620a030cb98bd72ba74c4c5a24c25
Alexander Lang <alex@upstream-berlin.com>
2009-03-17 22:03:48 +0100

    moved gitifnore to top dir, dried up couch_foo's Page model by using the view method to generate/query the word counts view

--- couch_foo/app/models/page.rb
+++ couch_foo/app/models/page.rb
@@ -12,24 +12,24 @@ class Page < CouchFoo::Base
   
   has_many :page_versions, :order => :version
   
+  
+  view :word_count,   "function(doc) {
+      if(doc.ruby_class == 'Page') {
+        var words = doc.body.split(/\\W/);
+        words.forEach(function(word) {
+          if (word.length > 0) emit(word, 1);
+        });
+      }
+    }", "function(key, combine) {
+        return sum(combine);
+      }", :group => true, :return_json => true
+  
   def versions
     page_versions
   end
   
   def self.word_counts
-    result = begin
-      database.view 'word_count/words', :group => true
-    rescue CouchFoo::DocumentNotFound
-      database.save({
-        "_id" => "_design/word_count",
-        :views => {
-          :words => word_count_views
-        }
-      })
-      retry
-    end
-    p result
-    result['rows'].map{|row| [row['key'], row['value']]}
+    word_count['rows'].map{|row| [row['key'], row['value']]}
   end
   
   private
@@ -38,20 +38,5 @@ class Page < CouchFoo::Base
     page_versions.create!(:body => body_was, :version => page_versions.size + 1) unless body_was.nil? || !body_changed?
   end
   
-  def self.word_count_views
-    {
-      :map => "function(doc) {
-        if(doc.ruby_class == 'Page') {
-          var words = doc.body.split(/\\W/);
-          words.forEach(function(word) {
-            if (word.length > 0) emit(word, 1);
-          });
-        }
-      }",
-      :reduce => "function(key, combine) {
-        return sum(combine);
-      }"
-    }
-  end
 
 end
